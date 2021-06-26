48cee0ce4807daafa42310cef206f25befada522
aashari <mq.aashari@gmail.com>
2021-06-27 00:12:26 +0800

    Update

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const hmacSHA512 = require('crypto-js/hmac-sha512');
 
 const IDX_PRIVATE_ENDPOINT = "https://indodax.com/tapi";
 const IDX_PUBLIC_ENDPOINT = "https://indodax.com/api";
+const VIP_PUBLIC_ENDPOINT = "https://vip.bitcoin.co.id/api";
 
 let IDX_KEY = process.env.IDX_KEY;
 let IDX_SECRET = process.env.IDX_SECRET;
 let IDX_IS_DEBUGGING = process.env.IDX_IS_DEBUGGING || false;
 let IDX_RESPONSE_TTL = process.env.IDX_RESPONSE_TTL || 0;
 let IDX_CACHE_DIRECTORY_PATH = process.env.IDX_CACHE_DIRECTORY_PATH || '/tmp/aashari/indodax-api-wrapper';
+
+process.env.VIP_TOTAL_API_CALLS = 0;
+process.env.IDX_TOTAL_API_CALLS = 0;
+process.env.IDX_START_TIMESTAMP = parseInt(Date.now() / 1000);
 
 let nodePersistent = null;
 
@@ -24,6 +29,7 @@ let _doPrivateRequestNonceHandler = (payload, cachedNonce) => {
     payloadQueryString = Object.keys(payload).map(key => `${key}=${payload[key]}`).join('&');
     encryptedPayload = hmacSHA512(payloadQueryString, IDX_SECRET).toString();
 
+    process.env.IDX_TOTAL_API_CALLS++;
     return axios.post(IDX_PRIVATE_ENDPOINT, payloadQueryString, {
         headers: {
             'Key': IDX_KEY,
@@ -69,6 +75,7 @@ let doPublicRequest = (pair, path) => {
         console.log("doPublicRequest", pair, path);
     }
 
+    process.env.IDX_TOTAL_API_CALLS++;
     return axios.post(`${IDX_PUBLIC_ENDPOINT}/${pair}/${path}`).then((succes) => {
 
         let responsePayload = succes.data;
@@ -87,12 +94,34 @@ let doPublicRequest = (pair, path) => {
 
 }
 
+let doPublicRequestVIP = (pair, path) => {
+
+    if (IDX_IS_DEBUGGING) {
+        console.log("doPublicRequestVIP", pair, path);
+    }
+
+    process.env.VIP_TOTAL_API_CALLS++;
+    return axios.get(`${VIP_PUBLIC_ENDPOINT}/${pair}/${path}`).then((succes) => {
+
+        let responsePayload = succes.data;
+
+        if (typeof responsePayload === 'string') {
+            responsePayload = responsePayload.replaceAll("'", '"');
+        }
+
+        if (responsePayload.error) {
+            throw new Error(responsePayload);
+        } else {
+            return storage.setItem(`doPublicRequestVIP@${pair}-${path}`, responsePayload).then(_ => responsePayload)
+        }
+
+    });
+
+}
+
 let doCachePublicRequest = (pair, path, disableCache = false) => {
     if (!nodePersistent) {
-        return storage.init({
-            dir: IDX_CACHE_DIRECTORY_PATH,
-            ttl: IDX_RESPONSE_TTL * 1000
-        }).then(response => {
+        return init().then(response => {
             nodePersistent = response;
             return doCachePublicRequest(pair, path, disableCache);
         });
@@ -106,12 +135,25 @@ let doCachePublicRequest = (pair, path, disableCache = false) => {
     });
 }
 
+let doCachePublicVIPRequest = (pair, path, disableCache = false) => {
+    if (!nodePersistent) {
+        return init().then(response => {
+            nodePersistent = response;
+            return doPublicRequestVIP(pair, path, disableCache);
+        });
+    }
+    return storage.getItem(`doPublicRequestVIP@${pair}-${path}`).then((responseCached) => {
+        if (responseCached && disableCache == false) {
+            if (IDX_IS_DEBUGGING) console.log("doPublicRequestVIP", pair, path)
+            return responseCached;
+        }
+        return doPublicRequestVIP(pair, path).then(responseRealTime => responseRealTime)
+    });
+}
+
 let doCachePrivateRequest = (payload, disableCache = false) => {
     if (!nodePersistent) {
-        return storage.init({
-            dir: IDX_CACHE_DIRECTORY_PATH,
-            ttl: IDX_RESPONSE_TTL * 1000
-        }).then(response => {
+        return init().then(response => {
             nodePersistent = response;
             return doCachePrivateRequest(payload, disableCache);
         });
@@ -125,8 +167,19 @@ let doCachePrivateRequest = (payload, disableCache = false) => {
     });
 }
 
+let init = () => {
+    return storage.init({
+        dir: IDX_CACHE_DIRECTORY_PATH,
+        ttl: IDX_RESPONSE_TTL * 1000
+    }).then(response => {
+        nodePersistent = response;
+        return response;
+    });
+}
 
 module.exports = {
+    init: init,
     privateRequest: doCachePrivateRequest,
-    publicRequest: doCachePublicRequest
+    publicRequest: doCachePublicRequest,
+    publicVIPRequest: doCachePublicVIPRequest
 }
